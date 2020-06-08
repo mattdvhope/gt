fa35a2fc9eb6f78b7ee13833cd958dc166244b04
mattdvhope <mattagape@me.com>
2020-06-08 17:55:43 +0700

    Text edit

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,13 @@ export default class footer extends Component {
       <div className="site-footer" id="footer">
         <div className="container">
           <span>{this.props.siteName}</span>&nbsp; | &nbsp;
-          <span><a href="https://www.privacypolicies.com/privacy/view/eafea5993b7a8f48855e35cc251a9442" target="_blank">Privacy Policy</a></span>
+          <span>
+            <a href="https://www.privacypolicies.com/privacy/view/eafea5993b7a8f48855e35cc251a9442"
+               target="_blank"
+               style={{ fontSize: `80%` }} >
+              Privacy Policy
+            </a>
+          </span>
         </div>
       </div>
     );
