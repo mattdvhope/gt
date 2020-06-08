09a3f712fa283d2c983fec1dcd3aafee0be7797e
mattdvhope <mattagape@me.com>
2020-06-08 14:58:21 +0700

    Working on FB login

--- src/components/layout.js
+++ src/components/layout.js
@@ -34,6 +34,33 @@ const Layout = ({ children, header }) => (
     render={data => (
       <>
         <HelmetLocale/>
+
+
+        <script
+          dangerouslySetInnerHTML={{ __html: `
+            window.fbAsyncInit = function() {
+              FB.init({
+                appId      : '2631589053776780',
+                xfbml      : true,
+                version    : 'v7.0'
+              });
+              FB.AppEvents.logPageView();
+            };
+
+            (function(d, s, id){
+               var js, fjs = d.getElementsByTagName(s)[0];
+               if (d.getElementById(id)) {return;}
+               js = d.createElement(s); js.id = id;
+               js.src = "https://connect.facebook.net/th_TH/sdk.js";
+               fjs.parentNode.insertBefore(js, fjs);
+             }(document, 'script', 'facebook-jssdk'));
+
+             console.log("loading SDK!!!!!!!!!!!!!!!!!!!!")
+
+           `}}
+        />
+
+
         <Header
           data={data.contentfulSiteInformation}
           siteTitle={data.contentfulSiteInformation.siteName}
