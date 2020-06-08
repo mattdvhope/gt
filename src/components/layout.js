39c4f2acc6688d3ca9ad9468d5675dfdfc25ff0a
mattdvhope <mattagape@me.com>
2020-06-08 10:16:12 +0700

    Adde FB SDK

--- src/components/layout.js
+++ src/components/layout.js
@@ -32,6 +32,30 @@ const Layout = ({ children, header }) => (
     
     render={data => (
       <>
+        <script
+          dangerouslySetInnerHTML={{ __html: `
+            window.fbAsyncInit = function() {
+              FB.init({
+                appId      : '961866087597091',
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v7.0'
+              });
+                
+              FB.AppEvents.logPageView();   
+                
+            };
+
+            (function(d, s, id){
+               var js, fjs = d.getElementsByTagName(s)[0];
+               if (d.getElementById(id)) {return;}
+               js = d.createElement(s); js.id = id;
+               js.src = "https://connect.facebook.net/th_TH/sdk.js";
+               fjs.parentNode.insertBefore(js, fjs);
+             }(document, 'script', 'facebook-jssdk'));
+          `}}
+        />
+
         <Header
           data={data.contentfulSiteInformation}
           siteTitle={data.contentfulSiteInformation.siteName}
