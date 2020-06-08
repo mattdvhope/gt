bda09fd5ace0dcaddbfa47b1621b377d967acd62
mattdvhope <mattagape@me.com>
2020-06-08 13:49:23 +0700

    Working on FB login

--- src/components/HelmetLocale.js
+++ src/components/HelmetLocale.js
@@ -8,24 +8,23 @@ export default () => (
   	<script
       dangerouslySetInnerHTML={{ __html: `
         window.fbAsyncInit = function() {
-          FB.init({
-            appId      : '2631589053776780',
-            cookie     : true,
-            xfbml      : true,
-            version    : 'v7.0'
-          });
-            
-          FB.AppEvents.logPageView();   
-            
-        };
+			    FB.init({
+			      appId      : '2631589053776780',
+			      xfbml      : true,
+			      version    : 'v7.0'
+			    });
+			    FB.AppEvents.logPageView();
+			  };
 
-        (function(d, s, id){
-           var js, fjs = d.getElementsByTagName(s)[0];
-           if (d.getElementById(id)) {return;}
-           js = d.createElement(s); js.id = id;
-           js.src = "https://connect.facebook.net/th_TH/sdk.js";
-           fjs.parentNode.insertBefore(js, fjs);
-         }(document, 'script', 'facebook-jssdk'));
+			  (function(d, s, id){
+			     var js, fjs = d.getElementsByTagName(s)[0];
+			     if (d.getElementById(id)) {return;}
+			     js = d.createElement(s); js.id = id;
+			     js.src = "https://connect.facebook.net/th_TH/sdk.js";
+			     fjs.parentNode.insertBefore(js, fjs);
+			   }(document, 'script', 'facebook-jssdk'));
+
+			   console.log("loading SDK!!!!!!!!!!!!!!!!!!!!")
 
        `}}
     />
