d7164f58ab1da83ce90e190376d289029073ddc5
mattdvhope <mattagape@me.com>
2020-06-10 16:55:41 +0700

    Added survey for createPage

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,10 +4,19 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators;
   return new Promise((resolve, reject) => {
     const blogPostTemplate = path.resolve("src/templates/blog-post.js");
+    const surveyPostTemplate = path.resolve("src/templates/survey-post.js");
     resolve(
       graphql(`
         {
           allContentfulBlogs(limit: 100) {
+            edges {
+              node {
+                id
+                slug
+              }
+            }
+          }
+          allContentfulSurveys(limit: 100) {
             edges {
               node {
                 id
@@ -20,6 +29,7 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
         if (result.errors) {
           reject(result.errors);
         }
+
         result.data.allContentfulBlogs.edges.forEach(edge => {
           createPage({
             path: edge.node.slug,
@@ -29,6 +39,17 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
             }
           });
         });
+
+        result.data.allContentfulSurveys.edges.forEach(edge => {
+          createPage({
+            path: edge.node.slug,
+            component: surveyPostTemplate,
+            context: {
+              slug: edge.node.slug
+            }
+          });
+        });
+
         return;
       })
     );
