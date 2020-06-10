1a25ad754bf67106de9294a708805e472a8f5357
mattdvhope <mattagape@me.com>
2020-06-10 16:55:22 +0700

    Removed FB comments

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,23 +13,6 @@ if (typeof window !== "undefined") {
   require("smooth-scroll")('a[href*="#"]');
 }
 
-// window.fbAsyncInit = function() {
-//   FB.init({
-//     appId      : '1153251771692328',
-//     xfbml      : true,
-//     version    : 'v7.0'
-//   });
-//   FB.AppEvents.logPageView();
-// };
-
-// (function(d, s, id){
-//    var js, fjs = d.getElementsByTagName(s)[0];
-//    if (d.getElementById(id)) {return;}
-//    js = d.createElement(s); js.id = id;
-//    js.src = "https://connect.facebook.net/th_TH/sdk.js";
-//    fjs.parentNode.insertBefore(js, fjs);
-//  }(document, 'script', 'facebook-jssdk'));
-
 const Layout = ({ children, header }) => (
   <StaticQuery
     query={graphql`
