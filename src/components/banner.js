f9428526b1def14565bf7857f127fab5dec14762
mattdvhope <mattagape@me.com>
2020-06-12 10:33:46 +0700

    Edit

--- src/components/banner.js
+++ src/components/banner.js
@@ -6,7 +6,8 @@ export default class Banner extends Component {
   render() {
     const { data } = this.props;
 
-    console.log(data.line)
+    const [count, setCount] = React.useState(0);
+    const [width, setWidth] = React.useState(600);
 
     return (
       <div className="banner">
