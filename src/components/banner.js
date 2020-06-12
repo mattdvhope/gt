6cbeefa9a2d91ed2facb9bd0c6932b1f8a7ccefc
mattdvhope <mattagape@me.com>
2020-06-12 10:32:51 +0700

    Edit

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+import { Link } from "gatsby";
 import Img from "gatsby-image";
 
 export default class Banner extends Component {
