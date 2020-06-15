24ca904d09689042e7d9ddb8b70e7e4138e4b28d
mattdvhope <mattagape@me.com>
2020-06-15 08:31:17 +0700

    Added HelmetLocale

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
 import "bootstrap/dist/css/bootstrap.css";
 
+import HelmetLocale from './HelmetLocale'
 import Header from "./header";
 import Footer from "./footer";
 
@@ -32,6 +33,7 @@ const Layout = ({ children, header }) => (
     
     render={data => (
       <>
+        <HelmetLocale />
         <Header
           data={data.contentfulSiteInformation}
           siteTitle={data.contentfulSiteInformation.siteName}
