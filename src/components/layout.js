c9c31f7aef7fd45edbec4bcc25dfb3ebee9fe7d0
mattdvhope <mattagape@me.com>
2020-06-15 13:59:08 +0700

    Edited FB div

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,46 +14,41 @@ if (typeof window !== "undefined") {
 }
 
 const Layout = ({ children, header }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        contentfulSiteInformation {
-          siteName
-          siteDescription
-          logo {
-            file {
-              url
+  <>
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          contentfulSiteInformation {
+            siteName
+            siteDescription
+            logo {
+              file {
+                url
+              }
             }
+            menus
           }
-          menus
         }
-      }
-    `}
-    
-    render={data => (
-      <>
-        <div id="fb-root"></div>
-        <script async defer
-          crossorigin="anonymous"
-          src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v7.0&appId=1153251771692328&autoLogAppEvents=1"
-          nonce="5eOUPX6a"
-        ></script>
-
-        <Header
-          data={data.contentfulSiteInformation}
-          siteTitle={data.contentfulSiteInformation.siteName}
-          header={header}
-        />
-        <div>
-          <main id="home">{children}</main>
-        </div>
-        <Footer siteName={data.contentfulSiteInformation.siteName} />
-        <script>
-          alert("Hello");
-        </script>
-      </>
-    )}
-  />
+      `}
+      
+      render={data => (
+        <>
+          <Header
+            data={data.contentfulSiteInformation}
+            siteTitle={data.contentfulSiteInformation.siteName}
+            header={header}
+          />
+          <div>
+            <main id="home">{children}</main>
+          </div>
+          <Footer siteName={data.contentfulSiteInformation.siteName} />
+          <script>
+            alert("Hello");
+          </script>
+        </>
+      )}
+    />
+  </>
 );
 
 Layout.propTypes = {
