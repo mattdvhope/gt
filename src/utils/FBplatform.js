313502d24d9709161e7dbdae7dd285f7e5af579e
mattdvhope <mattagape@me.com>
2020-06-16 12:41:40 +0700

    Abstracted makeState to file

--- src/utils/FBplatform.js
+++ src/utils/FBplatform.js
@@ -1,3 +1,5 @@
+import { makeState } from './makeState';
+
 // This is URL provided by FB Platform for logging into this app via FB.
 export const fbLoginURL = () => {
   return `
@@ -7,14 +9,4 @@ export const fbLoginURL = () => {
     //https%3A%2F%2Fwin-the-city.netlify.app%2Fsurvey-1%2F
 
     // https://www.facebook.com/v7.0/dialog/oauth?client_id=1153251771692328&redirect_uri=${process.env.GATSBY_API_URL}&state=${makeState(10)}
-}
-
-const makeState = (length) => {
-  let result           = '';
-  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charactersLength = characters.length;
-  for ( let i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
 }
--- src/utils/linePlatform.js
+++ src/utils/linePlatform.js
@@ -1,14 +1,6 @@
+import { makeState } from './makeState';
+
 // This is URL provided by LINE Platform for logging into this app via LINE.
 export const lineLoginURL = () => {
   return `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1654318519&redirect_uri=${process.env.GATSBY_API_URL}&state=${makeState(10)}&scope=profile%20openid&max_age=360000&ui_locales=th&bot_prompt=aggressive`
-}
-
-const makeState = (length) => {
-  let result           = '';
-  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charactersLength = characters.length;
-  for ( let i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
 }
