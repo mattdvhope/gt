bbb38cde85df64fd953fa07a31b980006f8ac97c
mattdvhope <mattagape@me.com>
2020-06-17 21:51:08 +0700

    Working on fb login

--- src/utils/FBLoginValidations.js
+++ src/utils/FBLoginValidations.js
@@ -10,6 +10,7 @@ export async function getIdToken(code) {
   const params = `client_id=1153251771692328&redirect_uri=${process.env.GATSBY_API_URL}&client_secret=8eef4a89221d26750c5309ab244dfe6f&code=${code}`;
   const response = await fetch(`https://graph.facebook.com/v7.0/oauth/access_token?${params}`, {
     method: 'GET',
+    mode: 'no-cors',
     headers: {'Content-Type':'application/json'}
   })
   // 'json' contains the various tokens provided by 'api.line.me/oauth2...'
