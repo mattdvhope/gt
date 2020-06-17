270b8c3e6af10750e42f963b72374c66b21bf23b
mattdvhope <mattagape@me.com>
2020-06-17 22:08:40 +0700

    Working on fb login

--- src/utils/FBplatform.js
+++ src/utils/FBplatform.js
@@ -6,7 +6,7 @@ export const fbLoginURL = () => {
     https://www.facebook.com/v7.0/dialog/oauth?client_id=1153251771692328&redirect_uri=https%3A%2F%2Fwin-the-city.netlify.app%2Fsurvey-1&state=${makeState(10)}&locale=th_TH
   `
     //https%3A%2F%2Fwww.xn--p3cbole1cb0dce80a.com%2Fsurvey-1%2F
-    //https%3A%2F%2Fwin-the-city.netlify.app%2Fsurvey-1%2F
+    //https%3A%2F%2Fwin-the-city.netlify.app%2Fsurvey-1%2F 
 
     // https://www.facebook.com/v7.0/dialog/oauth?client_id=1153251771692328&redirect_uri=${process.env.GATSBY_API_URL}&state=${makeState(10)}
 }
