e4ed232627c3ad7dced2616c5d0d79adfb30baa6
mattdvhope <mattagape@me.com>
2020-06-17 22:17:30 +0700

    Working on fb login

--- src/utils/FBLoginValidations.js
+++ src/utils/FBLoginValidations.js
@@ -8,12 +8,12 @@ export async function getIdToken(code) {
 
 	// const params = `grant_type=authorization_code&code=${code}&redirect_uri=${process.env.GATSBY_API_URL}&client_id=1654318519&client_secret=26e02bf88250345262e5d0cf7aab03f8`;
   const params = `client_id=1153251771692328&redirect_uri=${process.env.GATSBY_API_URL}&client_secret=8eef4a89221d26750c5309ab244dfe6f&code=${code}`;
-  const response = await fetch(`https://graph.facebook.com/v7.0/oauth/access_token?${params}`, {
-    method: 'GET',
-    headers: {'Content-Type':'application/json'}
-  })
+  // const response = await fetch(`https://graph.facebook.com/v7.0/oauth/access_token?${params}`, {
+  //   method: 'GET',
+  //   headers: {'Content-Type':'application/json'}
+  // })
 
-  // const response = await fetch(`https://graph.facebook.com/v7.0/oauth/access_token?${params}`)
+  const response = await fetch(`https://graph.facebook.com/v7.0/oauth/access_token?${params}`)
 
   // 'json' contains the various tokens provided by 'api.line.me/oauth2...'
   // const json = await response.json();
