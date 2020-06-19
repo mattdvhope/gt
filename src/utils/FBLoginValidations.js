9eacb43b4e73dc415ac24e3c381e788288472c63
mattdvhope <mattagape@me.com>
2020-06-20 05:10:03 +0700

    Working on FB login

--- src/utils/FBLoginValidations.js
+++ src/utils/FBLoginValidations.js
@@ -18,7 +18,25 @@ export async function inspectAccessToken(token) {
 
 export async function getUserProfile(user_id, token) {
   const response = await fetch(`https://graph.facebook.com/v7.0/${user_id}/picture?redirect=0&height=200&width=200&type=normal&access_token=${token}`)
-  // const response = await fetch(`https://graph.facebook.com/v7.0/${user_id}&access_token=${token}`)
   const json = await response.json();
   console.log(json);
 }
+
+
+
+// export const validateIdToken = (json) => {
+//   const base64Url = json.id_token.split('.')[1]; // json.id_token you get
+//   const base64 = base64Url.replace('-', '+').replace('_', '/');
+//   return JSON.parse(Buffer.from(base64, 'base64').toString('binary'));
+// }
+
+// export const checkValidation = (surveyPost, json, person, decodedData) => {
+//   if (JSON.stringify(person) === JSON.stringify(decodedData)) {
+//     handleLogin(person)
+//     addVisit(person.name, person.picture) // Record page visit in Rails
+//     surveyPost.setState({ person: person, id_token: json.id_token });
+//   } else {
+//     alert("ข้อมูลรับรองของคุณไม่ผ่านการตรวจสอบ");
+// 		logout(() => navigate(`/`))
+//   }
+// }
