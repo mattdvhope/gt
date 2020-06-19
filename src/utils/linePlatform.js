e89b2f20819bec77be4962f998bfdde85d9ed08a
mattdvhope <mattagape@me.com>
2020-06-19 15:48:34 +0700

    Env var

--- src/utils/linePlatform.js
+++ src/utils/linePlatform.js
@@ -2,5 +2,5 @@ import { makeState } from './makeState';
 
 // This is URL provided by LINE Platform for logging into this app via LINE. 
 export const lineLoginURL = () => {
-  return `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${process.env.LINE_LOGIN_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&state=${makeState(10)}&scope=profile%20openid&max_age=360000&ui_locales=th&bot_prompt=aggressive`
+  return `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${process.env.GATSBY_LINE_LOGIN_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&state=${makeState(10)}&scope=profile%20openid&max_age=360000&ui_locales=th&bot_prompt=aggressive`
 }
