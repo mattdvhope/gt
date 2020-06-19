b67cfd1fe811338a81da489a61fa1c89868e1ecb
mattdvhope <mattagape@me.com>
2020-06-20 05:24:33 +0700

    Working on FB login

--- src/utils/FBLoginValidations.js
+++ src/utils/FBLoginValidations.js
@@ -18,7 +18,7 @@ export async function inspectAccessToken(token) {
 
 export async function getUserProfile(user_id, token) {
   // const response = await fetch(`https://graph.facebook.com/v7.0/${user_id}/picture?redirect=0&height=200&width=200&type=normal&access_token=${token}`)
-  const response = await fetch(`https://graph.facebook.com/v7.0/${user_id}?access_token=${token}`)
+  const response = await fetch(`https://graph.facebook.com/v7.0/${user_id}/likes?access_token=${token}`)
   const json = await response.json();
   console.log(json);
 }
