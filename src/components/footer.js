12bc5392f705974a64a8e553dd8661af9c934d7d
mattdvhope <mattagape@me.com>
2020-06-19 15:06:14 +0700

    Removed comments

--- src/components/footer.js
+++ src/components/footer.js
@@ -27,34 +27,3 @@ export default class footer extends Component {
     );
   }
 }
-
-
-
-// export default class footer extends Component {
-//   render() {
-  
-//     const logout = isLoggedIn() ? (<Logout />) : privacy
-
-//     console.log("in footer, isLoggedIn? ..", isLoggedIn())
-
-
-//     const privacy = (
-//       <span>
-//         <a href="https://www.privacypolicies.com/privacy/view/eafea5993b7a8f48855e35cc251a9442"
-//            target="_blank"
-//            style={{ fontSize: `80%` }} >
-//           Privacy Policy
-//         </a>
-//       </span>
-//     )
-
-//     return (
-//       <div className="site-footer" id="footer">
-//         <div className="container">
-//           <span>{this.props.siteName}</span>&nbsp; | &nbsp;
-//         </div>
-//         {logout}
-//       </div>
-//     );
-//   }
-// }
