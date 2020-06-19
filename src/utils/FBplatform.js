f1bf9345e38c785757665a69d7150cf413adc761
mattdvhope <mattagape@me.com>
2020-06-19 15:54:02 +0700

    Env var

--- src/utils/FBplatform.js
+++ src/utils/FBplatform.js
@@ -3,6 +3,6 @@ import { makeState } from './makeState';
 // This is URL provided by FB Platform for logging into this app via FB.
 export const fbLoginURL = () => {
   return `
-    https://www.facebook.com/v7.0/dialog/oauth?client_id=1153251771692328&redirect_uri=${process.env.GATSBY_API_URL}&state=${makeState(10)}&locale=th_TH
+    https://www.facebook.com/v7.0/dialog/oauth?client_id=${process.env.GATSBY_FB_LOGIN_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&state=${makeState(10)}&locale=th_TH
   `
 }
