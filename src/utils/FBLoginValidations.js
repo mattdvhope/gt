71208495daa5c5712ab2e89534383f12099ce0c8
mattdvhope <mattagape@me.com>
2020-06-20 09:46:18 +0700

    Working on FB app

--- src/utils/FBLoginValidations.js
+++ src/utils/FBLoginValidations.js
@@ -6,7 +6,6 @@ export async function getAccessToken(code) {
   const params = `client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&client_secret=8eef4a89221d26750c5309ab244dfe6f&code=${code}`;
   const response = await fetch(`https://graph.facebook.com/v7.0/oauth/access_token?${params}`)
   const json = await response.json();
-  console.log("First GRAPH response:", json)
   return json.access_token;
 }
 
@@ -14,13 +13,14 @@ export async function inspectAccessToken(token) {
   const params = `input_token=${token}&access_token=${token}`;
   const response = await fetch(`https://graph.facebook.com/v7.0/debug_token?${params}`)
   const json = await response.json();
+  console.log("Debugged GRAPH response:", json)
   return json;
 }
 
 export async function getUserName(user_id, token) {
   const response = await fetch(`https://graph.facebook.com/v7.0/${user_id}?access_token=${token}`)
   const json = await response.json();
-  // console.log(json)
+  console.log("User GRAPH response:", json)
   return json;
 }
 
