4c8eb46a8567d276cf33dd7c3419baff1a17bad6
mattdvhope <mattagape@me.com>
2020-06-20 13:35:30 +0700

    Working on FB

--- src/components/FbLoginButton.js
+++ src/components/FbLoginButton.js
@@ -38,9 +38,10 @@ class FbLoginButton extends React.Component {
     console.log('Welcome!  Fetching your information.... ');
     window.FB.api('/me', function(response) {
     console.log('Successful login for: ' + response.name);
-    document.getElementById('status').innerHTML =
-      'Thanks for logging in, ' + response.name + '!';
-    });
+    console.log("Response:", response)
+    // document.getElementById('status').innerHTML =
+    //   'Thanks for logging in, ' + response.name + '!';
+    // });
   }
 
   statusChangeCallback(response) {
