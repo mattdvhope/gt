c695f20fc82291bfe005dbb7793a92e03fab84ed
mattdvhope <mattagape@me.com>
2020-06-20 14:48:15 +0700

    Working on FB

--- src/templates/SurveyPostPage.js
+++ src/templates/SurveyPostPage.js
@@ -43,22 +43,6 @@ const SurveyPostPage = ({ data, siteurl, socialConfigss, profile }) => (
               __html: data.description.childMarkdownRemark.html
             }}
           />
-          <div
-            className="fb-post"
-            data-href="https://www.facebook.com/relationshipsthailand/posts/119138423168533"
-            data-show-text="true"
-            data-width=""
-          >
-            <blockquote
-              cite="https://developers.facebook.com/relationshipsthailand/posts/119138423168533"
-              className="fb-xfbml-parse-ignore"
-            >
-              <p>Nice to Visit!</p>
-              Posted by <a href="https://www.facebook.com/relationshipsthailand/">สัมพันธภาพ</a> on&nbsp;<a href="https://developers.facebook.com/relationshipsthailand/posts/119138423168533">Friday, June 19, 2020</a>
-            </blockquote>
-          </div>
-          
-
           <div className="fb-comments" data-href="https://relationshipsthailand.org/survey-1" data-numposts="2" data-width=""></div>
         </div>
         <Form questions={rubyQuestions(data.questions)} />
