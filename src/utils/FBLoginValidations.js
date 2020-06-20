5adb12768c4965db98fa23cd7c825fbe64312889
mattdvhope <mattagape@me.com>
2020-06-20 09:37:09 +0700

    Removed comments

--- src/utils/FBLoginValidations.js
+++ src/utils/FBLoginValidations.js
@@ -29,22 +29,3 @@ export async function getUserPicture(user_id, token) {
   // console.log(json)
   return json;
 }
-
-
-
-// export const validateIdToken = (json) => {
-//   const base64Url = json.id_token.split('.')[1]; // json.id_token you get
-//   const base64 = base64Url.replace('-', '+').replace('_', '/');
-//   return JSON.parse(Buffer.from(base64, 'base64').toString('binary'));
-// }
-
-// export const checkValidation = (surveyPost, json, person, decodedData) => {
-//   if (JSON.stringify(person) === JSON.stringify(decodedData)) {
-//     handleLogin(person)
-//     addVisit(person.name, person.picture) // Record page visit in Rails
-//     surveyPost.setState({ person: person, id_token: json.id_token });
-//   } else {
-//     alert("ข้อมูลรับรองของคุณไม่ผ่านการตรวจสอบ");
-// 		logout(() => navigate(`/`))
-//   }
-// }
