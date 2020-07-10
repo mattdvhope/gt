e3428ee9ae17ad1f380505425882bf99bcd85fd9
mattdvhope <mattagape@me.com>
2020-07-10 15:10:49 +0700

    Local storage for FB login link

--- src/utils/FBLoginValidations.js
+++ src/utils/FBLoginValidations.js
@@ -3,7 +3,8 @@ import { logout, handleLogin } from "./auth"
 import { addVisit } from "./railsVisits"
 
 export async function getAccessToken(code) {
-  const params = `client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&client_secret=8eef4a89221d26750c5309ab244dfe6f&code=${code}`;
+  // const params = `client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&client_secret=8eef4a89221d26750c5309ab244dfe6f&code=${code}`;
+  const params = `client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&client_secret=8eef4a89221d26750c5309ab244dfe6f&grant_type=client_credentials`
   const response = await fetch(`https://graph.facebook.com/v7.0/oauth/access_token?${params}`)
   const json = await response.json();
 alert(JSON.stringify(json))
@@ -21,6 +22,5 @@ alert(JSON.stringify(json))
 export async function getUserProfile(user_id, token) {
   const response = await fetch(`https://graph.facebook.com/v7.0/${user_id}?fields=id,name,first_name,middle_name,last_name,name_format,picture,short_name,email&access_token=${token}`)
   const json = await response.json();
-alert(JSON.stringify(json))
   return json;
 }
