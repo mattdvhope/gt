d2aeccadeefe4cc91e80f06f9f16d91e51695650
mattdvhope <mattagape@me.com>
2020-07-29 10:51:03 +0700

    Update fb credentials

--- src/components/HelmetLocale.js
+++ src/components/HelmetLocale.js
@@ -1,14 +1,12 @@
 import React from "react"
 import { Helmet } from 'react-helmet';
 
-const fb_app = `https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v7.0&appId=611958516129057&autoLogAppEvents=1`
-
 export default () => (
   <Helmet>
     <div id="fb-root"></div>
     <script async defer
       crossorigin="anonymous"
-      src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v7.0&appId=611958516129057&autoLogAppEvents=1"
+      src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v7.0&appId=1153251771692328&autoLogAppEvents=1"
       nonce="8n79q7Mj"
     />
   </Helmet>
