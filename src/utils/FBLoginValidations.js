32ef97e9189029ce7324dde20669819d4af8c978
mattdvhope <mattagape@me.com>
2020-07-29 11:03:59 +0700

    Update fb credentials

--- src/utils/FBLoginValidations.js
+++ src/utils/FBLoginValidations.js
@@ -3,14 +3,14 @@ import { logout, handleLogin } from "./auth"
 import { addVisit } from "./railsVisits"
 
 export async function getAccessToken(code) {
-  const params = `client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&client_secret=8eef4a89221d26750c5309ab244dfe6f&code=${code}`;
+  const params = `client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&client_secret=${process.env.GATSBY_FB_APP_SECRET}&code=${code}`;
   const response = await fetch(`https://graph.facebook.com/v7.0/oauth/access_token?${params}`)
   const json = await response.json();
   return json.access_token;
 }
 
 export async function getAppAccessToken() {
-  const params = `client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&client_secret=8eef4a89221d26750c5309ab244dfe6f&grant_type=client_credentials`
+  const params = `client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&client_secret=${process.env.GATSBY_FB_APP_SECRET}&grant_type=client_credentials`
   const response = await fetch(`https://graph.facebook.com/v7.0/oauth/access_token?${params}`)
   const json = await response.json();
   return json.access_token;
