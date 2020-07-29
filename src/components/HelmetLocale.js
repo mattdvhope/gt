78a46763c7ccc333f025439ddf463eee04450461
mattdvhope <mattagape@me.com>
2020-07-29 10:37:46 +0700

    Update fb credentials

--- src/components/HelmetLocale.js
+++ src/components/HelmetLocale.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Helmet } from 'react-helmet';
 
-const fb_app = `https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v7.0&appId=${process.env.GATSBY_FB_APP_CLIENT_ID}&autoLogAppEvents=1`
+const fb_app = `https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v7.0&appId=611958516129057&autoLogAppEvents=1`
 
 export default () => (
   <Helmet>
