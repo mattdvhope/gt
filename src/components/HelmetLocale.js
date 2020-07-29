be77c93ee6a42f4fdbb3748e366113604501c853
mattdvhope <mattagape@me.com>
2020-07-29 10:38:20 +0700

    Update fb credentials

--- src/components/HelmetLocale.js
+++ src/components/HelmetLocale.js
@@ -8,7 +8,7 @@ export default () => (
     <div id="fb-root"></div>
     <script async defer
       crossorigin="anonymous"
-      src={fb_app}
+      src="https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v7.0&appId=611958516129057&autoLogAppEvents=1"
       nonce="8n79q7Mj"
     />
   </Helmet>
