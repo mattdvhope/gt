6676589dec9ea6c4b4086de94c3d01b007d5e1b3
Matt Malone <mattagape@me.com>
2021-02-11 14:18:59 +0700

    Added Graphql schema

--- plugins/source-plugin/gatsby-node.js
+++ plugins/source-plugin/gatsby-node.js
@@ -1,5 +1,39 @@
-// constants for your GraphQL Post and Author types
+const { ApolloClient } = require("apollo-client")
+const { InMemoryCache } = require("apollo-cache-inmemory")
+const { split } = require("apollo-link")
+const { HttpLink } = require("apollo-link-http")
+const { WebSocketLink } = require("apollo-link-ws")
+const { getMainDefinition } = require("apollo-utilities")
+const fetch = require("node-fetch")
+const gql = require("graphql-tag")
+const WebSocket = require("ws")
+
 const POST_NODE_TYPE = `Post`
+const AUTHOR_NODE_TYPE = `Author`
+
+const client = new ApolloClient({
+  link: split(
+    ({ query }) => {
+      const definition = getMainDefinition(query)
+      return (
+        definition.kind === "OperationDefinition" &&
+        definition.operation === "subscription"
+      )
+    },
+    new WebSocketLink({
+      uri: `ws://localhost:3000`, // or `ws://gatsby-source-plugin-api.glitch.me/`
+      options: {
+        reconnect: true,
+      },
+      webSocketImpl: WebSocket,
+    }),
+    new HttpLink({
+      uri: `http://localhost:3000`, // or `https://gatsby-source-plugin-api.glitch.me/`
+      fetch,
+    })
+  ),
+  cache: new InMemoryCache(),
+})
 
 exports.sourceNodes = async ({
   actions,
@@ -9,12 +43,28 @@ exports.sourceNodes = async ({
 }) => {
   const { createNode } = actions
 
-  const data = {
-    posts: [
-      { id: 1, description: `Hello world!` },
-      { id: 2, description: `Second post!` },
-    ],
-  }
+	const { data } = await client.query({
+	  query: gql`
+	    query {
+	      posts {
+	        id
+	        description
+	        slug
+	        imgUrl
+	        imgAlt
+	        author {
+	          id
+	          name
+	        }
+	      }
+	      authors {
+	        id
+	        name
+	      }
+	    }
+	  `,
+	})
+
 
   // loop through data and create Gatsby nodes
   data.posts.forEach(post =>
@@ -27,6 +77,20 @@ exports.sourceNodes = async ({
         type: POST_NODE_TYPE,
         content: JSON.stringify(post),
         contentDigest: createContentDigest(post),
+      },
+    })
+  )
+
+  data.authors.forEach(author =>
+    createNode({
+      ...author,
+      id: createNodeId(`${AUTHOR_NODE_TYPE}-${author.id}`), // hashes the inputs into an ID
+      parent: null,
+      children: [],
+      internal: {
+        type: AUTHOR_NODE_TYPE,
+        content: JSON.stringify(author),
+        contentDigest: createContentDigest(author),
       },
     })
   )
